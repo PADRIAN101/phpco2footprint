90ccb231461cccd546cc6001549c6d4509416ec6
PADRIAN101 <janadrian11224@gmail.com>
2025-04-20 16:23:22 -0400

    commit

---
+++ cli.php
@@ -0,0 +1,21 @@
+<?php
+
+$driver = 'mysql';
+
+$config = http_build_query(data: [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'phpco2footprint'
+], arg_separator: ';');
+
+$dsn = "{$driver}:{$config}";
+$username = 'root';
+$password = '';
+
+try {
+    $db = new PDO($dsn, $username, $password);
+} catch (PDOException $e) {
+    die("Unable to connect to database");
+}
+
+echo "Connected to database";
