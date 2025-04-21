5dc10e9238693b0bcb7f8f387bbdeee0a866d0aa
PADRIAN101 <janadrian11224@gmail.com>
2025-04-21 13:03:02 -0400

    commit

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-use Framework\{TemplateEngine, Database};
+use Framework\{TemplateEngine, Database, Container};
 use App\Config\Paths;
-use App\Services\ValidatorService;
+use App\Services\{ValidatorService, UserService};
 
 
 
@@ -12,7 +12,7 @@ return [
     TemplateEngine::class => fn() => new TemplateEngine(Paths::VIEW),
     ValidatorService::class => fn() => new ValidatorService(),
     Database::class => fn() => new Database(
-        $_ENV['DRIVER'],
+        $_ENV['DB_DRIVER'],
         [
             'host' => $_ENV['DB_HOST'],
             'port' => $_ENV['DB_PORT'],
@@ -20,5 +20,10 @@ return [
         ],
         $_ENV['DB_USER'],
         $_ENV['DB_PASS']
-    )
+    ),
+    UserService::class => function (Container $container) {
+        $db = $container->get(Database::class);
+
+        return new UserService($db);
+    }
 ];
